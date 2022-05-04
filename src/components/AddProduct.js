0e8394e04e65d813800cc804cdb821e61c4565ed
sachinbadnikai <38239908+sachinbadnikai@users.noreply.github.com>
2022-05-04 18:30:20 +0530

    Update AddProduct.js

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -26,7 +26,7 @@ const AddProduct = () => {
                     />
                 </div>
                 <div className="field">
-                    <button className="button is-primary">Save button</button>
+                    <button className="button is-primary">Save b</button>
                 </div>
             </form>
         </div>
